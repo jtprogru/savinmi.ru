c804c12d6eb8551e89cac0385ca32c7eccd35a94
Michael Savin <m.savin@corp.mail.ru>
2021-07-07 17:57:19 +0300

    Сделать возможность скачивания резюме #13 - Теперь меня устраивает всё
    Signed-off-by: Michael Savin <m.savin@corp.mail.ru>

--- src/js/printer.js
+++ src/js/printer.js
@@ -1,9 +1,18 @@
 function printer() {
-  const content = window.document.querySelector('#content');
-  const opt = {
-    filename: 'MSavin_cv.pdf',
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' },
-    pagebreak: { after: '.pdf-break-after', before: '.pdf-break-before', avoid: 'hr.m-0.first'}
-  };
-  html2pdf().set(opt).from(content).save();
+    const content = window.document.querySelector('#content');
+    const opt = {
+        margin:       0,
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        filename:     'CV_Savin_MI.pdf',
+        enableLinks:  true,
+        jsPDF:        { unit: 'mm', format: 'A4', orientation: 'landscape' },
+        pagebreak: {
+            mode:     'legacy',
+            after:    '.pdf-break-after',
+            before:   '.pdf-break-before',
+            avoid:    '.social-icon'
+        }
+    };
+    html2pdf().set(opt).from(content).save();
 }
