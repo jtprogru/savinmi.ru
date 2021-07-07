379dd56d95d4a6ca1eb2adfe07f2413e4e9c7e7d
Michael Savin <m.savin@corp.mail.ru>
2021-07-07 20:03:33 +0300

    Поправил скрипт генерации PDF
    Signed-off-by: Michael Savin <m.savin@corp.mail.ru>

--- src/js/printer.js
+++ src/js/printer.js
@@ -3,15 +3,22 @@ function printer() {
     const opt = {
         margin:       0,
         image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
+        html2canvas:  {
+            scale: 2
+        },
         filename:     'CV_Savin_MI.pdf',
         enableLinks:  true,
-        jsPDF:        { unit: 'mm', format: 'A4', orientation: 'landscape' },
+        jsPDF:        {
+            unit: 'in',
+            format: 'letter',
+            orientation: 'landscape',
+            hotfixes: ['px_scaling']
+        },
         pagebreak: {
             mode:     'legacy',
             after:    '.pdf-break-after',
             before:   '.pdf-break-before',
-            avoid:    '.social-icon'
+            avoid:    ['.social-icon', '.fa-li', '#content'],
         }
     };
     html2pdf().set(opt).from(content).save();
