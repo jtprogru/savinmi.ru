70867f84a8520548771ee3816be58ad5e1d61dfe
Michael Savin <jtprogru@gmail.com>
2023-07-05 14:56:53 +0300

    upd: Fix rendering PDF

--- scripts/render-pdf.js
+++ scripts/render-pdf.js
@@ -5,7 +5,7 @@ module.exports = async function curlPdf() {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto('https://savinmi.ru', {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({ format: 'A4', scale: 0.6 });
+    const pdf = await page.pdf({ format: 'A4', scale: 0.82, landscape: true });
 
     await browser.close();
     return pdf
